32427e00186be097acc77ff0b16d7ace77b15c9b
ewenpch <85692168+ewenpch@users.noreply.github.com>
2026-01-04 17:32:49 +0100

    Add mouse look and options autoload to player
    Implemented mouse look controls in player.gd using a new options.gd autoload for mouse sensitivity. Updated Player.tscn to set up camera environment, and main.tscn to add a level base. Registered options.gd as an autoload in project.godot.

--- classes/player.gd
+++ classes/player.gd
@@ -6,6 +6,21 @@ var inventory:Inventory = Inventory.new()
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 
+@onready var head:Node3D = $head
+@onready var camera:Camera3D = $head/Camera3D
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("pause") :
+		get_tree().quit()
+
+func _ready() -> void:
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		head.rotate_y(-event.relative.x * options.MOUSE_SENS)
+		camera.rotate_x(-event.relative.y * options.MOUSE_SENS)
+		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-80), deg_to_rad(80))
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -15,7 +30,7 @@ func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("left", "right", "forward", "backward")
-	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var direction := (head.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
---
+++ options.gd
@@ -0,0 +1,3 @@
+extends Node
+
+var MOUSE_SENS:float = 0.015
